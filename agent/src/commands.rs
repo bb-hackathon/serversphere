d39c55afdf5b27d4839babe4b09af4a43589ff24
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-11-23 00:38:19 +0300

    fix(agent): Shutdown node from a different thread

--- agent/src/commands.rs
+++ agent/src/commands.rs
@@ -1,12 +1,21 @@
 use axum::http::StatusCode;
 use nix::sys::reboot::RebootMode;
+use std::{convert::Infallible, io, thread, time::Duration};
 use tracing::instrument;
 
-#[instrument]
-pub async fn reboot() -> Result<(), StatusCode> {
-    tracing::debug!(message = "handling reboot request");
-    let _ = nix::sys::reboot::reboot(RebootMode::RB_AUTOBOOT)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+const REBOOT_DELAY: u64 = 500;
 
-    Ok(())
+#[instrument]
+pub async fn reboot() -> Result<&'static str, StatusCode> {
+    tracing::debug!(message = "handling reboot request");
+
+    let message = format!("spawning reboot thread, should fire in {REBOOT_DELAY}ms");
+    tracing::debug!(message);
+    thread::spawn(move || -> Result<Infallible, io::Error> {
+        thread::sleep(Duration::from_millis(REBOOT_DELAY));
+        let _ = nix::sys::reboot::reboot(RebootMode::RB_AUTOBOOT)?;
+        unreachable!("system should be rebooting at this point");
+    });
+
+    Ok("node will reboot soon")
 }
